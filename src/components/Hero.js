17a57f8149ea65885059766265f50105dc82cf10
eddiemetrium <eddiemitch262@gmail.com>
2025-02-05 10:22:24 -0500

    almost done

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -28,7 +28,7 @@ export default function Hero() {
           </svg>
         </div>
         <div className="content">
-          <h1>collect & sell<br /><span>super rare nfts</span></h1>
+          <h1>collect & sell<br /><span>kiboko rare nfts</span></h1>
 
           <div className="container panel">
             <div className="row">
